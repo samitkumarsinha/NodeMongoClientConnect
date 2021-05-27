f98fa2a860747ad013c34b8e203c042685633438
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 08:38:15 +0530

    port change

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('hello world');
+    res.end('hello world..');
 }).listen(port);
