c47a7f65be19e9b55954e2bd3177275db7597dd6
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 09:55:22 +0530

    add mongoclient

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('hello world..');
-}).listen(port);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://unicorn1:unicorn1@cluster0.idow2.mongodb.net/samnode";
+
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("sample_weatherdata");
+    dbo.collection("data").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type' : 'text/plain'});
+//     res.end('hello world..');
+// }).listen(port);
